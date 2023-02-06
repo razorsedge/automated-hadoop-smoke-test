b8befff335f6a8f63d200c568f1e188eae5709b7
Michael Arnold <github@razorsedge.org>
2023-02-06 09:41:13 -0500

    Run shfmt on hdfsTest.sh.

--- bin/hdfsTest.sh
+++ bin/hdfsTest.sh
@@ -51,7 +51,7 @@ done
 # main
 if [ -z "$HDFS" ]; then
   # shellcheck disable=SC2128
-  if [[ $BASH_SOURCE = */* ]]; then
+  if [[ $BASH_SOURCE == */* ]]; then
     cd -- "${BASH_SOURCE%/*}/" || exit
   fi
   # shellcheck source=/dev/null
@@ -62,39 +62,63 @@ echo "HDFS_PATH: $HDFS_PATH"
 echo "LOC_PATH: $LOC_PATH"
 echo "TEMP_PATH: $TEMP_PATH"
 
-if [[ "$_ACTION" == "all" ]] || [[ "$_ACTION" == "test" ]]; then
+if [[ $_ACTION == "all" ]] || [[ $_ACTION == "test" ]]; then
   hdfs dfs -ls /
-  rc=$?; if [[ $rc != 0 ]]; then echo "Error in showing files in HDFS! exiting"; echo " - HDFS         - Failed [Error in showing files in HDFS]" >> "$LOG_PATH"/SummaryReport.txt; exit $rc; fi
+  rc=$?
+  if [[ $rc != 0 ]]; then
+    echo "Error in showing files in HDFS! exiting"
+    echo " - HDFS         - Failed [Error in showing files in HDFS]" >>"$LOG_PATH"/SummaryReport.txt
+    exit $rc
+  fi
 
   hdfs dfs -mkdir -p "$HDFS_PATH"
-  rc=$?; if [[ $rc != 0 ]]; then echo "Error in showing files in HDFS! exiting"; echo " - HDFS         - Failed [Error in showing files in HDFS]" >> "$LOG_PATH"/SummaryReport.txt; exit $rc; fi
+  rc=$?
+  if [[ $rc != 0 ]]; then
+    echo "Error in showing files in HDFS! exiting"
+    echo " - HDFS         - Failed [Error in showing files in HDFS]" >>"$LOG_PATH"/SummaryReport.txt
+    exit $rc
+  fi
 
   hdfs dfs -put "$LOC_PATH" "$HDFS_PATH"
-  rc=$?; if [[ $rc != 0 ]]; then echo "Error in copying file to HDFS! exiting"; echo " - HDFS         - Failed [Error in copying file to HDFS]" >> "$LOG_PATH"/SummaryReport.txt; exit $rc; fi
+  rc=$?
+  if [[ $rc != 0 ]]; then
+    echo "Error in copying file to HDFS! exiting"
+    echo " - HDFS         - Failed [Error in copying file to HDFS]" >>"$LOG_PATH"/SummaryReport.txt
+    exit $rc
+  fi
 
   hdfs dfs -get "$HDFS_PATH" "$TEMP_PATH"
-  rc=$?; if [[ $rc != 0 ]]; then echo "Error in copying file from HDFS! exiting"; echo " - HDFS         - Failed [Error in copying file from HDFS]" >> "$LOG_PATH"/SummaryReport.txt; exit $rc; fi
+  rc=$?
+  if [[ $rc != 0 ]]; then
+    echo "Error in copying file from HDFS! exiting"
+    echo " - HDFS         - Failed [Error in copying file from HDFS]" >>"$LOG_PATH"/SummaryReport.txt
+    exit $rc
+  fi
 
   cat "$TEMP_PATH"/hosts
-  rc=$?; if [[ $rc != 0 ]]; then echo "Error in showing copied file from HDFS! exiting"; echo " - HDFS         - Failed [Error in showing copied file from HDFS]" >> "$LOG_PATH"/SummaryReport.txt; exit $rc; fi
+  rc=$?
+  if [[ $rc != 0 ]]; then
+    echo "Error in showing copied file from HDFS! exiting"
+    echo " - HDFS         - Failed [Error in showing copied file from HDFS]" >>"$LOG_PATH"/SummaryReport.txt
+    exit $rc
+  fi
 
   cmp "$LOC_PATH" "$TEMP_PATH"/"${LOC_PATH##/*/}"
   status=$?
-  if [[ $status = 0 ]]; then
+  if [[ $status == 0 ]]; then
     echo "Files are the same"
-    echo " - HDFS         - Passed" >> "$LOG_PATH"/SummaryReport.txt
+    echo " - HDFS         - Passed" >>"$LOG_PATH"/SummaryReport.txt
     echo "**************************************"
     echo "* HDFS test completed Successfully ! *"
     echo "**************************************"
   else
     echo "Files are different"
-    echo " - HDFS         - Failed[Files are different]" >> "$LOG_PATH"/SummaryReport.txt
+    echo " - HDFS         - Failed[Files are different]" >>"$LOG_PATH"/SummaryReport.txt
     echo "**********************"
     echo "* HDFS test Failed ! *"
     echo "**********************"
   fi
-elif [[ "$_ACTION" == "all" ]] || [[ "$_ACTION" == "clean" ]]; then
+elif [[ $_ACTION == "all" ]] || [[ $_ACTION == "clean" ]]; then
   hdfs dfs -rm -r "$HDFS_PATH"
   rm -f -r "$TEMP_PATH"
 fi
-
