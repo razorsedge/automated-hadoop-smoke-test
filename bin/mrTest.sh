3f04490c90f0ebd610ecd25221607cee8e24848c
Michael Arnold <github@razorsedge.org>
2023-02-06 11:55:23 -0500

    Improve the test logic in mrTest.sh.
    No need to delete, create, and again delete directories in HDFS.

--- bin/mrTest.sh
+++ bin/mrTest.sh
@@ -56,6 +56,9 @@ if [ -z "$MAPREDUCE" ]; then
   fi
   # shellcheck source=/dev/null
   source ../conf/SmokeConfig.config
+  MAP_REDUCE_SRC=../lib/WordCountFile.txt
+else
+  MAP_REDUCE_SRC=lib/WordCountFile.txt
 fi
 
 echo "MAP_REDUCE_IN: $MAP_REDUCE_IN"
@@ -63,13 +66,8 @@ echo "MAP_REDUCE_OUT: $MAP_REDUCE_OUT"
 echo "MAP_REDUCE_JAR: $MAP_REDUCE_JAR"
 
 if [[ $_ACTION == "all" ]] || [[ $_ACTION == "test" ]]; then
-  hdfs dfs -rm -r -f "$MAP_REDUCE_IN"
-  hdfs dfs -rm -r -f "$MAP_REDUCE_OUT"
-
   hdfs dfs -mkdir -p "$MAP_REDUCE_IN"
-  hdfs dfs -mkdir -p "$MAP_REDUCE_OUT"
-
-  hdfs dfs -put -f ./lib/WordCountFile.txt "$MAP_REDUCE_IN"/
+  hdfs dfs -put -f "$MAP_REDUCE_SRC" "$MAP_REDUCE_IN"
   rc=$?
   if [[ $rc != 0 ]]; then
     echo "Input data tranfser failed! exiting"
@@ -77,9 +75,7 @@ if [[ $_ACTION == "all" ]] || [[ $_ACTION == "test" ]]; then
     exit $rc
   fi
 
-  hdfs dfs -rm -r "$MAP_REDUCE_OUT"
-
-  yarn jar "$MAP_REDUCE_JAR" wordcount "$MAP_REDUCE_IN"/WordCountFile.txt "$MAP_REDUCE_OUT"
+  yarn jar "$MAP_REDUCE_JAR" wordcount "$MAP_REDUCE_IN"WordCountFile.txt "$MAP_REDUCE_OUT"
   rc=$?
   if [[ $rc != 0 ]]; then
     echo "Mapreduce Job failed! exiting"
@@ -87,12 +83,10 @@ if [[ $_ACTION == "all" ]] || [[ $_ACTION == "test" ]]; then
     exit $rc
   fi
 
+  echo " - MapReduce    - Passed" >>"$LOG_PATH"/SummaryReport.txt
   echo "******************************************"
   echo "* MapReduce test completed Successfully! *"
   echo "******************************************"
-
-  echo " - MapReduce    - Passed" >>"$LOG_PATH"/SummaryReport.txt
 elif [[ $_ACTION == "all" ]] || [[ $_ACTION == "clean" ]]; then
-  hdfs dfs -rm -r "$MAP_REDUCE_IN"/WordCountFile.txt
-  hdfs dfs -rm -r "$MAP_REDUCE_OUT"
+  hdfs dfs -rm -r "$MAP_REDUCE_IN" "$MAP_REDUCE_OUT"
 fi
